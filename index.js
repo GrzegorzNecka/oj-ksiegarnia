38aa8f9b0679a28723283b8f82cfba50638d3340
GrzegorzNecka <strzypior@gmail.com>
2021-01-09 15:52:05 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-import './style.css';
+import "./style.css";
 
-const API_URL = 'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=harry%20potter'
+const API_URL =
+  "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=harry%20potter";
+
+const formToSearch = document.querySelector(".form");
+const inputTitle = document.getElementById("#book-name");
+
+
+
+
+
+// fetch(API_URL, {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json"
+//   },
+//   body: JSON.stringify()
+// })
+//   .then(response => {
+//     if (response.ok) {
+//       return response.text();
+//     }
+//     throw "Nie udało się wysłać zapytania!";
+//   })
+//   .then(responseText => {
+//     console.log(responseText);
+//   })
+//   .catch(err => {
+//     alert("spróbuj ponownie");
+//   });
