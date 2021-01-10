e36605e55ca8e58da17e064647806a2eb398824c
GrzegorzNecka <strzypior@gmail.com>
2021-01-10 13:44:45 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,36 +4,49 @@ const API_URL = "https://www.goodreads.com/book/auto_complete?format=json&q=";
 
 const formToSearch = document.querySelector(".form");
 const inputTitle = document.getElementById("#book-name");
-const q = "franz%20kafka";
+const q = "harry%20potter";
 const search_url = `${PROXY}${API_URL}${q}`;
 
 async function getData(url = "") {
-  const response = await fetch(search_url, {
+  const response = await fetch(url, {
     method: "GET", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "default", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
+    credentials: "omit", // include, *same-origin, omit
     headers: { "Content-Type": "application/json" },
     redirect: "follow", // manual, *follow, error
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify()
   });
 
+  console.log(response);
   return response.json();
 }
 
-getData(search_url)
+
+
+
+const test = (url) => getData(url)
   .then(data => {
     console.log(data);
   })
   .catch(err => {
-    alert("coś poszło nie tak");
+    console.log("coś poszło nie tak");
   });
+
+
+//const result = inventory.find( ({ name }) => name === 'cherries' );
 
 formToSearch.addEventListener("submit", e => {
   e.preventDefault();
 
-  console.log(e);
+  const { 0: input } = e.target;
 
-  
+  input.value;
+
+test(search_url)
+
+  console.log(input.value);
+
+
 });
