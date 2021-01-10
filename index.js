7120e0190dd1d059e53729c993e18ab677f251a0
GrzegorzNecka <strzypior@gmail.com>
2021-01-10 15:59:47 +0100

    1 file modified, 2 added, 2 deleted

--- index.js
+++ index.js
@@ -39,13 +39,7 @@ function createListBooks(b) {
 async function getData(url = "") {
   createLoader();
   const response = await fetch(url, {
-    method: "GET", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "default", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "omit", // include, *same-origin, omit
-    headers: { "Content-Type": "application/json" },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    method: "GET", 
     body: JSON.stringify()
   });
 
